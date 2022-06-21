c21913f660e88419bedd724a4373b4e373d7ab80
Brayan Betancourt Ramirez <67015417+Betancourt1996@users.noreply.github.com>
2022-06-20 23:40:34 -0500

    corregido scripts

--- js/ejercisio.js
+++ js/ejercisio.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
+  
     cargarDatos=()=>{
         //alert('Alerta');
         let url = "https://dataserverdaw.herokuapp.com/escritores/xml";
@@ -7,7 +8,6 @@ document.addEventListener('DOMContentLoaded',()=>{
             .then(data =>{
                 const parser = new DOMParser();
                 const xml = parser.parseFromString(data, "application/xml");
-                //console.log(xml);
                 let escritores = xml.getElementsByTagName('escritor')
 
                 for(let escritor of escritores){
@@ -23,18 +23,43 @@ document.addEventListener('DOMContentLoaded',()=>{
     };
     window.addEventListener('DOMContentLoaded',(event)=>{
         cargarDatos();
-    })
-    document.getElementsByTagName('select').addEventListener('change',()=>{
-        fetch("https://dataserverdaw.herokuapp.com/escritores/frases")
-        .then(response => response.json())
-        .then(data => {
-            //console.log(data);
-            for (const key in response){
-                if(obj.hasOwnProperty(key)){
-                  console.log(`${key} : ${response[key]}`)
-                } }
-        })
-        .catch(console.error);
         
+    })
+    let select = document.getElementsByName('select')[0]
+    select.addEventListener('change',()=>{
+        (async () => {
+            try {
+              let divFrases = document.getElementById('frases')  
+              divFrases.innerHTML = "";
+              const response = await fetch("https://dataserverdaw.herokuapp.com/escritores/frases");
+              const data = await response.json();
+                
+              Object.entries(data['frases']).forEach(([key, value]) => {
+                if (value['id_autor'] == select.value){                  
+                                   
+                  let nombre = select.options[select.selectedIndex].textContent
+                  let frase = value['texto']
+
+                  let plantilla = `
+                  <div class="col-lg-3">
+                  <div class="test-inner ">
+                      <div class="test-author-thumb d-flex">
+                          <div class="test-author-info">
+                              <h4>${nombre}</h4>                                            
+                          </div>
+                      </div>
+                      <span>${frase}</span>
+                      <i class="fa fa-quote-right"></i>
+                  </div>
+                  </div>
+                  `
+                  divFrases.innerHTML += plantilla
+                }                
+              });
+
+            } catch (err) {
+              console.error(err);
+            }
+          })();
     })
 })
